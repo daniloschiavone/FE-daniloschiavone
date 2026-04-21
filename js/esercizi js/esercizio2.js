abab046bb7c15bc3cb676cfbee1ac1124dd3ec1b
daniloschiavone <Daniloschiavone91@gmail.com>
2026-04-21 16:55:44 +0200

    esercizio2 2.2

--- js/esercizi js/esercizio2.js
+++ js/esercizi js/esercizio2.js
@@ -6,3 +6,15 @@ let nome = "Danilo";
 const annoNascita = 1991;
 console.log(nome);
 console.log(annoNascita);
+
+// ES 2.2 Dichiara due variabili numeriche a e b. Stampa il risultato della loro addizione, sottrazione, moltiplicazione, divisione e modulo.
+console.log("ESERCIZIO 2.2");
+
+let a = 10;
+let b = 5;
+console.log("Addizione:", a + b);
+console.log("Sottrazione:", a - b);
+console.log("Moltiplicazione:", a * b);
+console.log("Divisione:", a / b);
+console.log("Modulo:", a % b);
+
