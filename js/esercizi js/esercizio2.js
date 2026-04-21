e11682e6dcf7f5e2cdb452d2cb7df86b94f76c87
daniloschiavone <Daniloschiavone91@gmail.com>
2026-04-21 16:54:00 +0200

    add esercizio 2.1 to declare a variable and a constant, printing their values

---
+++ js/esercizi js/esercizio2.js
@@ -0,0 +1,8 @@
+// ES 2.1 Dichiara una variabile nome con il tuo nome e una costante annoNascita con il tuo anno di nascita. Stampa entrambi i valori.
+
+console.log("ESERCIZIO 2.1"); 
+
+let nome = "Danilo";
+const annoNascita = 1991;
+console.log(nome);
+console.log(annoNascita);
