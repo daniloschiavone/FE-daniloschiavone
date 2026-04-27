fb8fe5f9b5bfc48ea753433fc1d2a8068a4309f7
daniloschiavone <Daniloschiavone91@gmail.com>
2026-04-27 14:22:21 +0200

    aggiornamento del file 4funzioni-esercizi.js con l'esercizio 4.1 completato

--- js/esercizi js/4funzioni-esercizi.js
+++ js/esercizi js/4funzioni-esercizi.js
@@ -1,5 +1,9 @@
 //Scrivi una arrow function benvenuto che accetti un nome come parametro e restituisca la stringa "Benvenuto, [nome]!".
+console.log("ESERCIZIO 4.1");
+
 const benvenuto = (nome) => {
     return `Benvenuto, ${nome}!`;
 }
 console.log(benvenuto("Mario"));
+
+//
