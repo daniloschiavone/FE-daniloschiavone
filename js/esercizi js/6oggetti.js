57ea8460a321dbe889ea8e04649aa229eb669ddf
daniloschiavone <Daniloschiavone91@gmail.com>
2026-05-05 14:55:13 +0200

    add esercizio 6.1: create book object and log properties

---
+++ js/esercizi js/6oggetti.js
@@ -0,0 +1,15 @@
+//Crea un oggetto libro con le proprietà: titolo, autore, annoPubblicazione, pagine. Stampa ogni proprietà.
+
+console.log("Esercizio 6.1");
+
+const libro = {
+    titolo: "Il Signore degli Anelli",
+    autore: "J.R.R. Tolkein",
+    annoPubblicazione: 1954,
+    pagine: 1216
+};
+
+console.log("Titolo:", libro.titolo);
+console.log("Autore:", libro.autore);
+console.log("Anno di pubblicazione:", libro.annoPubblicazione);
+console.log("Pagine:", libro.pagine);
