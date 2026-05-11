8d712aa4afab11abad3eb66e7f0dae3931abf473
daniloschiavone <Daniloschiavone91@gmail.com>
2026-05-11 13:47:11 +0200

    es 7.2

--- js/esercizi js/7metodi-array-avanzati.js
+++ js/esercizi js/7metodi-array-avanzati.js
@@ -4,3 +4,13 @@ console.log("Esercizio 7.1:");
 const numeri = [1, 2, 3, 4, 5];
 const numeriTriplicati = numeri.map(n => n * 3);
 console.log(numeriTriplicati);
+
+//Dato un array di stringhe, 
+//usa map per creare un nuovo array dove ogni stringa ha la prima lettera maiuscola e il resto minuscolo.
+
+console.log("Esercizio 7.2:");
+
+const stringhe = ["ciao", "mondo", "javascript"];
+const stringheCapitalizzate = stringhe.map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase());
+console.log(stringheCapitalizzate);
+
