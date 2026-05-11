45fef4e825d453896dd46d5f78ceef78bf1db658
daniloschiavone <Daniloschiavone91@gmail.com>
2026-05-11 13:46:13 +0200

    es 7.1

---
+++ js/esercizi js/7metodi-array-avanzati.js
@@ -0,0 +1,6 @@
+//Dato un array di numeri, usa map per creare un nuovo array dove ogni numero è triplicato.
+console.log("Esercizio 7.1:");
+
+const numeri = [1, 2, 3, 4, 5];
+const numeriTriplicati = numeri.map(n => n * 3);
+console.log(numeriTriplicati);
